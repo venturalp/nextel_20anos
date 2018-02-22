67bfaea44971e5072494672f26f2b357fb126180
Guilherme Ventura <venturalp@gmail.com>
2018-02-22 02:55:28 -0300

    animação

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function(){
 	angular.module("nextelApp", ['slickCarousel'])
 })();
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
+    } 
+  });
+});
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -7,7 +7,7 @@
 			autoplaySpeed: 6000,
 			dots: true
 		};
-		
+
 		$scope.currentPlano = 0;
 
 		$scope.configPlanos = {
@@ -23,11 +23,53 @@
 			appendDots: '#dots-planos',
 			event: {
 				afterChange: function (event, slick, currentSlide, nextSlide) {
-					console.log(currentSlide);
 					$scope.currentPlano = currentSlide;
 				}
 			}
 		};
+		
+		$scope.produtos = [
+			{
+				img: 'images/ico-roaming.png',
+				txt: 'Roaming Nacional Grátis'
+			},
+			{
+				img: 'images/ico-ligacoes.png',
+				txt: 'Ligações DDD para qualquer operadora e com o código 99'
+			},
+			{
+				img: 'images/ico-sms.png',
+				txt: 'Pacote SMS'
+			}
+		]
+		
+		$scope.currentProduct = 0;
+		var animaProdutos = setInterval(function(){
+			if ($scope.currentProduct == $scope.produtos.length)
+				$scope.currentProduct = 0;
+			else
+				$scope.currentProduct++;			
+		}, 3000)
+
+		document.onkeydown = function (event) {
+			event = event || window.event;
+			if (event.keyCode == 27) {
+				if ($scope.popup.open)
+					$scope.closePopup();
+			}
+		}
+
+		$scope.popup = {}
+		$scope.openPopup = function () {
+			$scope.popup.open = true;
+			$scope.popup.titulo = 'Lorem Ipsum';
+			$scope.popup.content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis dui justo, ac lobortis lacus feugiat ut. Integer elit augue, placerat ut libero eget, facilisis ornare nunc. Vestibulum laoreet erat sed posuere varius. Morbi malesuada sem quam, sed dignissim tellus posuere vel. Fusce a sagittis mi. Sed efficitur purus nibh. Sed auctor felis vel turpis eleifend ultrices. Fusce dictum pretium malesuada. In nec consectetur libero. Vestibulum imperdiet turpis vel tempor molestie. Proin luctus ante vitae vestibulum sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. In nisi sem, porta eget consequat nec, cursus a nunc. In vel nunc augue.';
+		}
+
+		$scope.closePopup = function () {
+			$scope.popup.open = false;
+			$scope.$apply();
+		}
 
 		$scope.plans = [];
 
@@ -40,7 +82,6 @@
 		}
 
 		api.getPlans(gotPlans, failPlans);
-		api.register();
 
 	}]);
 })();
